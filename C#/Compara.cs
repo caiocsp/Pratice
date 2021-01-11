35ede67d45583e7ec2d2ba1dda7e53723f567e52
caiocsp <caio.c.paulino@gmail.com>
2021-01-11 14:36:54 -0300

    update Compara.cs

--- C#/Compara.cs
+++ C#/Compara.cs
@@ -5,12 +5,13 @@ public class Compara
 	//Compara duas strings para ver se é um palíndromo
    public static void Main()
    {
+
       string value1 = Console.ReadLine();
       char[] value2 = value1.ToCharArray();
 
-      Console.WriteLine(value1);
+      //Utilizando a função de classe array para inverter o array de char
       Array.Reverse(value2);
-      string palindromo = new string(value2);
+      string palindromo = new string(value2);//Passando como referência para uma string para a compararação
       Console.WriteLine(palindromo);
 
       if(value1.Replace(" ", "") != palindromo.Replace(" ", "")){
@@ -18,6 +19,6 @@ public class Compara
       }
       else{
       	Console.WriteLine("É um palíndromo");
-  	  }
+      }
    }
 }
