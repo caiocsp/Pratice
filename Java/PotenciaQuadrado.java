6add6632a43b8b0bdf667f67a94c2650b59986b1
caiocsp <caio.c.paulino@gmail.com>
2021-01-11 16:20:49 -0300

    Java pratice

---
+++ Java/PotenciaQuadrado.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;//Potenciação ao quadrado de um valor do array
+
+public class PotenciaQuadrado{
+
+	public static void main(String[] args){
+
+		 int[] arr = new int[5];//Vetor declarado vazio
+		 int i, resposta;
+
+		 Scanner entrada = new Scanner(System.in);
+
+		 System.out.println("Digite os valores do array:");
+		 for(i = 0; i < 5; i++){
+		 	arr[i] = entrada.nextInt();
+		 }
+		 System.out.println("Valores do array:");
+		 for(i = 0; i < 5; i++){
+		 	System.out.println(arr[i]);
+		 }
+
+		 System.out.print("Digite o indice do segundo array para potenciacao (entre 0 e 4): ");
+		do{
+		 i =  entrada.nextInt();
+		}while(i > 4 && i <= 0);
+
+		System.out.println("Resposta: "+ Math.pow(arr[i], 2));
+	}
+
+}
